6ea7cd7cdc7c36b9f0aae43552da3508f14483f7
yunser <>
2019-06-24 00:21:15 +0800

    backup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# npm run build
+npm run build
 scp -r ./dist/* root@120.78.177.9:/usr/local/nginx/clipboard-front
--- src/config/index.js
+++ src/config/index.js
@@ -5,15 +5,14 @@ if (process.env.NODE_ENV === 'production') {
     domain = {
         api: 'https://nodeapi.yunser.com',
         qrcode: 'https://nodeapi.yunser.com',
-        ws: 'wss://nodeapi.yunser.com'
+        ws: 'wss://nodeapi.yunser.com/clipboard'
     }
 } else {
     domain = {
         api: 'http://localhost:1026',
         qrcode: 'https://nodeapi.yunser.com',
-        // ws: 'wss://nodeapi.yunser.com' // 400
+        ws: 'wss://nodeapi.yunser.com/clipboard'
         // ws: 'ws://localhost:1026/clipboard' // 400
-        ws: 'ws://localhost:1026/classroom' // 400
     }
 }
 
